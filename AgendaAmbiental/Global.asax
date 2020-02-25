856c2571f8c6e6017403f12ce43fd0d10b3cda98
jhony2703 <jc.pv.27@gmail.com>
2020-02-24 18:13:27 -0600

    Encabezado
    Le puse el encabezado

---
+++ AgendaAmbiental/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AgendaAmbiental.MvcApplication" Language="C#" %>
---
+++ AgendaAmbiental/Models/login.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AgendaAmbiental.Models
+{
+    public class login
+    {
+        public int user { get; set; }
+
+        public string password { get; set; }
+    }
+}
